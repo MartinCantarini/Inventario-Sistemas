cd228e960a9b4b27690aa7cccacd4287cfa41f9b
Martin Cantarini <md.cantarini@gmail.com>
2017-05-22 12:14:26 -0300

    22-05-17 - Se arreglo tabla 'Items asociados a esta compra' en purchases#show

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,6 +1,9 @@
 class Model < ApplicationRecord
 	validates :nombre, presence: true
 	def self.busqueda_modelo(search)
-  		where("nombre LIKE ?", "%#{search}%") 
+  		where("nombre LIKE ?", "%#{search}%")
+	end
+	def self.getNombre(id_modelo)
+		return Model.find(id_modelo).nombre
 	end
 end
